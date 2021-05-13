6cf44923f68f6e1a0b57af79e2758b56000781b0
James English <64441712+anglicus@users.noreply.github.com>
2021-05-13 09:39:02 -0700

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -3,5 +3,11 @@ const emailError = document.querySelector(".email-error");
 
 emailInput.addEventListener('invalid', e => {
     e.preventDefault();
+    if (emailInput.validity.valueMissing) {
+        emailError.textContent = "Oops! Please add your email";
+    }
+    if (emailInput.validity.typeMismatch) {
+        emailError.textContent = "Oops! Please check your email";
+    }
     emailError.classList.add('show-error');
 });
